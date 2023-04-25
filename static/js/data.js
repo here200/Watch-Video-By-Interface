a1923b4761b6b973a14aa6b6db53b1b8f560a27d
ice036 <ice036@qq.com>
2023-04-25 14:52:58 +0800

    update parse video router

--- static/js/data.js
+++ static/js/data.js
@@ -1,12 +1,12 @@
 const routes = [
   {name: 'BL解析', url: 'https://svip.bljiex.cc/?v='}, 
   {name: '虾米解析', url: 'https://jx.xmflv.com/?url='},
-  {name: 'MAQQ', url: 'https://player.maqq.cn/?url='},
+  {name: 'JSON解析', url: 'https://jx.jsonplayer.com/player/?url='},
+  {name: 'OK解析', url: 'https://okjx.cc/?url='},
   {name: '影猫の仓库 ', url: 'https://search.ymck.me/index.php?q='}, 
 ]
 
 const friends = [
   {name: 'Here200\'s Blog', url: 'https://here200.github.io/'},
-  {name: '够低调解析', url: 'http://www.gddyu.com/'},
   {name: '热播之家', url: 'https://rebozj.com/'},
 ]
