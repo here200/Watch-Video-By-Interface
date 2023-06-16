3e07f159aeba0729747331835430c13e6c8e6f04
ice036 <ice036@qq.com>
2023-06-16 18:58:38 +0800

    remove not useful links

--- static/js/data.js
+++ static/js/data.js
@@ -2,11 +2,9 @@ const routes = [
   {name: 'BL解析', url: 'https://svip.bljiex.cc/?v='}, 
   {name: '虾米解析', url: 'https://jx.xmflv.com/?url='},
   {name: 'JSON解析', url: 'https://jx.jsonplayer.com/player/?url='},
-  {name: 'OK解析', url: 'https://okjx.cc/?url='},
   {name: '影猫の仓库 ', url: 'https://search.ymck.me/index.php?q='}, 
 ]
 
 const friends = [
   {name: 'Here200\'s Bookmark', url: 'https://here200.github.io/Bookmark/'},
-  {name: '热播之家', url: 'https://rebozj.com/'},
 ]
