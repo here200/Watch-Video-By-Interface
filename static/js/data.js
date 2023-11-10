da5886873f982f9d3ecf74fc0c5b3a6438c60f7a
ice036 <ice036@qq.com>
2023-11-10 14:16:32 +0800

    update

--- static/js/data.js
+++ static/js/data.js
@@ -2,7 +2,7 @@ const routes = [
   {name: 'BL解析', url: 'https://svip.bljiex.cc/?v='}, 
   {name: '虾米解析', url: 'https://jx.xmflv.com/?url='},
   {name: 'JSON解析', url: 'https://jx.jsonplayer.com/player/?url='},
-  {name: '影猫の仓库 ', url: 'https://search.ymck.me/index.php?q='}, 
+  {name: '影猫の仓库 ', url: 'https://ymck.pro/search.html?wd='}, 
 ]
 
 const friends = [
